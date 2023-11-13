baa131b6ee374875fbe1122051beb2917b77f35c
fardilalif <muhdfardilalif@gmail.com>
2023-11-13 19:30:45 +0800

    finished lorem-ipsum project

--- 08-lorem-ipsum/starter/src/App.jsx
+++ 08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,42 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import data from "./data.js";
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleCount = (e) => {
+    setCount(parseInt(e.target.value));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText(data.slice(0, parseInt(count)));
+  };
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="paragraph">paragraphs: </label>
+        <input
+          type="number"
+          name="paragraph"
+          id="paragraph"
+          min="1"
+          max="8"
+          step="1"
+          value={count}
+          onChange={handleCount}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
